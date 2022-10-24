a0467f27bf923a3c513f5e8bb951a6686c26daf9
chase-manning <chase@manning.dev>
2022-10-24 22:04:02 +0100

    :sparkles: create supported by component

---
+++ src/components/SupportedBy.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import Section from "./Section";
+
+const StyledSupportedBy = styled.div`
+  display: flex;
+  font-size: 2rem;
+`;
+
+const SupportedBy = () => {
+  return (
+    <Section header="Supported By">
+      <StyledSupportedBy>
+        Gnosis, Stateful Works, 1Inch, Gitcoin
+      </StyledSupportedBy>
+    </Section>
+  );
+};
+
+export default SupportedBy;
