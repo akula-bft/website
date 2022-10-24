e012128eef79aae9a7dec0c4bc690bcc46a6dbf2
chase-manning <chase@manning.dev>
2022-10-24 22:03:27 +0100

    :sparkles: create benefits component

---
+++ src/components/Benefits.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import Section from "./Section";
+
+const StyledBenefits = styled.div`
+  display: flex;
+  font-size: 2rem;
+`;
+
+const Benefits = () => {
+  return (
+    <Section header="Benefits">
+      <StyledBenefits>
+        Much lower disk footprint, Faster sync speed, Crash resilience,
+        Extremely fast historical RPC queries
+      </StyledBenefits>
+    </Section>
+  );
+};
+
+export default Benefits;
