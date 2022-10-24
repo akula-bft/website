f8951f316644faad48c577eff0481428fe31c85e
chase-manning <chase@manning.dev>
2022-10-24 20:30:09 +0100

    :lipstick: add styling to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import styled from "styled-components";
+
+const StyledApp = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background: var(--bg);
+`;
+
 const App = () => {
-  return <div>meow</div>;
+  return <StyledApp>Akula Website</StyledApp>;
 };
 
 export default App;
