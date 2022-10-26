514380e0029e9ebee1f359db1efe84f3594a5b74
chase-manning <chase@manning.dev>
2022-10-26 23:18:11 +0100

    :rocket: add hero text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,12 +9,22 @@ const StyledHero = styled.div`
   flex-direction: column;
   padding: 0 14rem;
   padding-top: 5rem;
+  align-items: center;
+`;
+
+const HeroText = styled.h1`
+  font-size: 9.6rem;
+  text-align: center;
+  max-width: 90rem;
+  margin-top: 10rem;
+  font-weight: 600;
 `;
 
 const Hero = () => {
   return (
     <StyledHero>
       <Header />
+      <HeroText>The Worlds Fastest Ethereum Client</HeroText>
       The name, a description, a button to the docs, some interesting
       illustration or something
     </StyledHero>
