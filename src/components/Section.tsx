ef668e123f9d962c19161554faa81ce1dae3a47d
chase-manning <chase@manning.dev>
2022-10-27 10:40:14 +0100

    :lipstick: style section header

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -3,15 +3,15 @@ import styled from "styled-components";
 const StyledSection = styled.div`
   display: flex;
   width: 100%;
-  padding: 10rem 0;
-  border: solid 1px pink;
   flex-direction: column;
   align-items: center;
 `;
 
 const Header = styled.h2`
-  font-size: 4rem;
-  margin-bottom: 4rem;
+  font-size: 9.6rem;
+  font-weight: 500;
+  margin-bottom: 8rem;
+  margin-top: 20rem;
 `;
 
 interface Props {
