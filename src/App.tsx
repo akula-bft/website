477e89d8dc91d6ee72bf98364d7544babd73e1aa
chase-manning <chase@manning.dev>
2022-10-28 10:48:14 +0100

    :rocket: move supporters above networks

--- src/App.tsx
+++ src/App.tsx
@@ -22,8 +22,8 @@ const App = () => {
       <Hero />
       <Benefits />
       <Performance />
-      <Networks />
       <SupportedBy />
+      <Networks />
       <Footer />
     </StyledApp>
   );
