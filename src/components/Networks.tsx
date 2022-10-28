1d6ca179ee987cf7ef8429521ebd3259fb8c6d32
chase-manning <chase@manning.dev>
2022-10-28 11:44:37 +0100

    :rocket: add networks

--- src/components/Networks.tsx
+++ src/components/Networks.tsx
@@ -1,16 +1,51 @@
 import styled from "styled-components";
 import Section from "./Section";
 
+const networks: string[] = [
+  "Mainnet",
+  "Sepolia",
+  "Goerli",
+  "Ropsten",
+  "Rinkeby",
+  "+ Custom networks",
+];
+
 const StyledNetworks = styled.div`
-  display: flex;
+  width: 100%;
+  row-gap: 10rem;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
   font-size: 2rem;
+  padding: 0 14rem;
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Network = styled.div`
+  background: var(--terminal);
+  border-radius: 1.5rem;
+  height: 9.5rem;
+  font-size: 2.4rem;
+  width: 30rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-transform: uppercase;
 `;
 
 const Networks = () => {
   return (
-    <Section id="networks" header="Supported Networks">
+    <Section id="networks" header="Networks">
       <StyledNetworks>
-        Ethereum, Sepolia, Goerli, Ropsten, Rinkeby
+        {networks.map((network, index) => (
+          <Container key={index}>
+            <Network>{network}</Network>
+          </Container>
+        ))}
       </StyledNetworks>
     </Section>
   );
