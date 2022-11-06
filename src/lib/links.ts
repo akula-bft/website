89ae92662ea3bd20bb9e96270832ecd52808f294
chase-manning <chase@manning.dev>
2022-11-06 14:07:55 +0000

    :sparkles: create links file

---
+++ src/lib/links.ts
@@ -0,0 +1,6 @@
+export const DOCS_LINK = "https://docs.akula.app/";
+export const GITHUB_LINK = "https://github.com/akula-bft/akula";
+export const TWITTER_LINK = "https://twitter.com/AkulaApp";
+export const DOCKER_LINK =
+  "https://hub.docker.com/repository/docker/vorot93/akula";
+export const TELEGRAM_LINK = "https://t.me/+Tp4PNRes9zkzMGUy";
