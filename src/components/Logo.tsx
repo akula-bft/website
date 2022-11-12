1b689e4c74483d076e55dbd1da5750e6b716d1d5
chase-manning <chase@manning.dev>
2022-11-12 18:35:36 +0000

    :iphone: mobile logo layout

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,11 +3,15 @@ import styled from "styled-components";
 const StyledLogo = styled.a`
   display: flex;
   cursor: pointer;
-  font-size: 4rem;
   font-weight: 600;
 
   :hover {
     text-decoration: none;
+  }
+
+  font-size: 4rem;
+  @media (max-width: 768px) {
+    font-size: 3.4rem;
   }
 `;
 
